d2fd911e23f860a22d1c9f6a2efdf532f2344858
john cravener <jcravener@outlook.com>
2025-02-17 13:26:23 -0800

    Init check in of c# JhcAdoRest

---
+++ src/JhcAdoRest/JhcAdoRest/cmdlets/Greeting.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Management.Automation;
+
+namespace JhcAdoRest.cmdlets
+{
+    [Cmdlet(VerbsCommon.Get, "Greeting")]
+    public class Greeting : Cmdlet
+    {
+        [Parameter(
+            Position = 0,
+            Mandatory = true,
+            ValueFromPipeline = true,
+            ValueFromPipelineByPropertyName = true)]
+        public string Name { get; set; }
+
+        protected override void ProcessRecord()
+        {
+            WriteObject($"Hello, {Name}!");
+        }
+    }
+}

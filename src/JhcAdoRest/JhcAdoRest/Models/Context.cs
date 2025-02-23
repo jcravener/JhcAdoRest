0d15f7dcd89e89e9ecf9635ad3fae8328b1b5ec6
john cravener <jcravener@outlook.com>
2025-02-23 11:46:47 -0800

    Fleshing out JhcAdoRestEnvironment functionality

---
+++ src/JhcAdoRest/JhcAdoRest/Models/Context.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JhcAdoRest.Models
+{
+    public class Context
+    {
+        public string? Organization { get; set; }
+
+        public string? Project { get; set; }
+
+        public SecureString? Pat { get; set; }
+    }
+}
---
+++ src/JhcAdoRest/JhcAdoRest/cmdlets/RestEnvironment.cs
@@ -0,0 +1,36 @@
+﻿using JhcAdoRest.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management.Automation;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JhcAdoRest.cmdlets
+{
+    [Cmdlet(VerbsCommon.Set, "RestEnvironment")]
+    public class RestEnvironment : PSCmdlet
+    {
+        [Parameter( Position = 0,Mandatory = true)]
+        public string? Organization { get; set; }
+
+        [Parameter(Position = 2, Mandatory = true)]
+        public string? Project { get; set; }
+
+        [Parameter(Position = 3, Mandatory = true)]
+        public SecureString? Pat { get; set; }
+
+        protected override void ProcessRecord() 
+        {
+            Context context = new Context
+            {
+                Organization = Organization,
+                Project = Project,
+                Pat = Pat
+            };
+
+            SessionState.PSVariable.Set("RestEnvironment", context);
+        }
+    }
+}

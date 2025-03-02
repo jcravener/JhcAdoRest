471642a5ca5a035649d01a5e3b6b6e334e2567fa
john cravener <jcravener@outlook.com>
2025-03-02 11:10:13 -0800

    Fleshing out Api client functionality

---
+++ src/JhcAdoRest/JhcAdoRest/Client/ApiClient.cs
@@ -0,0 +1,67 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JhcAdoRest.Client
+{
+    public class ApiClient : IApiClient
+    {
+        private readonly HttpClient _httpClient;
+
+        public ApiClient(HttpClient httpClient, string personalAccessToken)
+        {
+            _httpClient = httpClient;
+
+            if (!string.IsNullOrWhiteSpace(personalAccessToken))
+            {
+                _httpClient.DefaultRequestHeaders.Authorization =
+                    new AuthenticationHeaderValue("Bearer", personalAccessToken);
+            }
+            else
+            {
+                throw new ArgumentException("PAT was null or empty", nameof(personalAccessToken));
+            }
+        }
+
+        public async Task<string> GetAsync(string endpoint)
+        {
+            var response = await _httpClient.GetAsync(endpoint);
+            response.EnsureSuccessStatusCode();
+            return await response.Content.ReadAsStringAsync();
+        }
+
+        public async Task<string> PostAsync(string endpoint, object payload)
+        {
+            var content = new StringContent(
+                JsonConvert.SerializeObject(payload),
+                Encoding.UTF8,
+                "application/json");
+
+            var response = await _httpClient.PostAsync(endpoint, content);
+            response.EnsureSuccessStatusCode();
+            return await response.Content.ReadAsStringAsync();
+        }
+
+        public async Task<string> PutAsync(string enpoint, object payload)
+        {
+            var content = new StringContent(
+                JsonConvert.SerializeObject(payload),
+                Encoding.UTF8,
+                "application/json");
+
+            var response = await _httpClient.PutAsync(enpoint, content);
+            response.EnsureSuccessStatusCode();
+            return await response.Content.ReadAsStringAsync();
+        }
+
+        public async Task DeleteAsync(string endpoint)
+        {
+            var response = await _httpClient.DeleteAsync(endpoint);
+            response.EnsureSuccessStatusCode();
+        }
+    }
+}
---
+++ src/JhcAdoRest/JhcAdoRest/Client/IApiClient.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JhcAdoRest.Client
+{
+    public interface IApiClient
+    {
+        Task<string> GetAsync(string endpoint);
+        Task<string> PostAsync(string endpoint, object payload);
+        Task<string> PutAsync(string endpoint, object payload);
+        Task DeleteAsync(string endpoint);
+    }
+}
